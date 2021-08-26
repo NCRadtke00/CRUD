c23a7fda601913033d71b248e9478efc1fd75f63
Nicholas Radtke <Raddnc@gmail.com>
2021-08-25 23:11:48 -0500

    connected db and created add and edit contollers(HttpPost). this is what the api is being called to do. in this case add and edit rows in the table studentmaster.

--- ReactCRUD/Controllers/DefaultController.cs
+++ ReactCRUD/Controllers/DefaultController.cs
@@ -4,14 +4,67 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using ReactCRUD.Models;
+using ReactCRUD;
 namespace ReactCRUD.Controllers
 {
     public class DefaultController : Controller
     {
+        private readonly CrudDemoEntities DB;
         // GET: Default
         public ActionResult Index()
         {
             return View();
+        }
+        [Route("AddotrUpdatestudent")]
+        [HttpPost]
+        public object AddotrUpdatestudent(Student st)
+        {
+            try
+            {
+                if (st.Id == 0)
+                {
+                    studentmaster sm = new studentmaster();
+                    sm.Name = st.Name;
+                    sm.RollNo = st.Rollno;
+                    sm.Address = st.Address;
+                    sm.Class = st.Class;
+                    DB.studentmasters.Add(sm);
+                    DB.SaveChanges();
+                    return new Response
+                    {
+                        Status = "Success",
+                        Message = "Data Successfully"
+                    };
+                }
+                else
+                {
+                    var obj = DB.studentmasters.Where(x => x.Id == st.Id).ToList().FirstOrDefault();
+                    if (obj.Id > 0)
+                    {
+
+                        obj.Name = st.Name;
+                        obj.RollNo = st.Rollno;
+                        obj.Address = st.Address;
+                        obj.Class = st.Class;
+                        DB.SaveChanges();
+                        return new Response
+                        {
+                            Status = "Updated",
+                            Message = "Updated Successfully"
+                        };
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex.Message);
+            }
+            return new Response
+            {
+                Status = "Error",
+                Message = "Data not insert"
+            };
+
         }
     }
 }
---
+++ ReactCRUD/Data/CrudDemoEntities.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using ReactCRUD.Models;
+
+public partial class CrudDemoEntities : DbContext
+{
+    public CrudDemoEntities() : base("name=CrudDemoEntities")
+    {
+    }
+    protected override void OnModelCreating(DbModelBuilder modelBuilder)
+    {
+        throw new UnintentionalCodeFirstException();
+    }
+    public virtual DbSet<studentmaster> studentmasters { get; set; }
+}
